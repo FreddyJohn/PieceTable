b7385df24db91ed8ff8bb3076f4644487640c3b3
FreddyJohn <39596344+FreddyJohn@users.noreply.github.com>
2021-10-05 17:53:05 -0400

    Update Edit.java
    add new field for edge case

--- Edit.java
+++ Edit.java
@@ -1,16 +1,21 @@
 package piecetable;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Nick
  */
-public class Edit{
+public class Edit implements Serializable{
     public int length;
     public int offset;
     public String editType;
-    public Edit(int length, int offset, String editType){
+    boolean in_added;
+    public Edit(int length, int offset,
+            String editType,boolean in_added){
         this.length = length;
         this.offset = offset;
         this.editType = editType;
+        this.in_added = in_added;
     }
 }
