0d0257909bba0674d9e0f383a6fbc229719103d6
FreddyJohn <39596344+FreddyJohn@users.noreply.github.com>
2021-10-05 17:48:03 -0400

    Update UnitTests.java
    add assert statements

--- UnitTests.java
+++ UnitTests.java
@@ -1,6 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package piecetable;
+
 import java.io.IOException;
+
 import java.io.RandomAccessFile;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,110 +16,100 @@ import java.util.logging.Logger;
  *
  * @author Nick
  * 
- * PieceTable modified to have only one buffer and to work with any
- * arbitrary byte stream
  * 
  */
+     
 public class UnitTests {
-    private static String path = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello1.txt";
-    private static String path1 = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello.txt";
-    private static String path2 = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello2.txt";
+    private static final String editsBufferPath = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello1.txt";
+    private static final String objectPath = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello.txt";
+    private static final String editsPath = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello3.txt";
+    private static final String originalBufferPath = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello2.txt";
+    private static final String removeBufferPath = "C:\\Users\\Nick\\Downloads\\audioResearch\\hello96.txt";
+
+    
+
+    private static PieceTableAPI piecetable;
+    private static RandomAccessFile edits;
+    private static RandomAccessFile original;
+    
     public static void main(String[] args) {
-       test_persist();
+
+       intialize();
        test_add_remove();
-    }
-     public static void test_add_remove(){
-        persist piecetable = new persist(path1,path,path2,3);
-        RandomAccessFile file = null;
-        RandomAccessFile edits = null;
-        try {
-            file = new RandomAccessFile(path2,"rw");
-            edits = new RandomAccessFile(path,"rw");
-            file.write("hellogoodbye".getBytes());
-        }
-        catch (IOException ex) {
-            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("adding original");
-        piecetable.add_original(12);
-        System.out.println(piecetable._text_len);
-        
-        System.out.println("removing a piece");
-        piecetable.remove(piecetable._text_len-10, 3);
-        
-        System.out.println(piecetable._text_len);
-        System.out.println(new String(piecetable.get_text()));
-        piecetable.print_pieces();
-        try {
-            edits.seek(edits.length());
-            edits.write("g".getBytes());
-        }
-        catch (IOException ex) {
-            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("adding a piece after removing one");
-        piecetable.add(1, 0,edits);
-
-        System.out.println(new String(piecetable.get_text()));
-        piecetable.print_pieces();
+       
+       intialize();
+       test_original_remove_add_find();
+       rigourously_test_undo_redo(piecetable,edits);
+       
+       intialize();
+       add_original_add_at_zero_add_add_at_zero();
+       
+       intialize();
+       test_undo_redo_original_then_more_edits();
 
     }
     
-    public static void test_persist(){
-        persist piecetable = new persist(path1,path,path2,3);
-        RandomAccessFile file = null;
-        RandomAccessFile edits = null;
+    public static void intialize(){
+        piecetable = new PieceTableAPI(objectPath,editsBufferPath,editsPath,originalBufferPath,removeBufferPath);
+        edits = null;
         try {
-            file = new RandomAccessFile(path2,"rw");
-            edits = new RandomAccessFile(path,"rw");
-            file.write("hellogoodbye".getBytes());
+            original = new RandomAccessFile(originalBufferPath,"rw");
+            edits = new RandomAccessFile(editsBufferPath,"rw");
+            edits.setLength(0);
+            original.setLength(0);
         }
         catch (IOException ex) {
             Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("adding original");
-        piecetable.add_original(12);
-        System.out.println(piecetable._text_len);
-        System.out.println(new String(piecetable.get_text()));
-        try {
-            edits.write("goodbye".getBytes());
-        }
-        catch (IOException ex) {
-            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("adding new piece");
-        piecetable.add(7,0,edits);
- 
-        System.out.println(piecetable._text_len);
-        System.out.println(new String(piecetable.get_text()));
-        System.out.println("removing a piece");
-        piecetable.remove(piecetable._text_len-10, 3);
-        System.out.println(new String(piecetable.get_text()));
-        piecetable.print_pieces();        
-        try {
-            edits.seek(edits.length());
-            edits.write("g".getBytes());
-        }
-        catch (IOException ex) {
-            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("adding a piece after removing one");
-        piecetable.add(1, (int) piecetable._text_len,edits);
-        System.out.println(new String(piecetable.get_text()));
-        piecetable.print_pieces();        
-
-        printFile(edits);
-        
+        } 
     }
     
-    public static void printFile(RandomAccessFile file){
+    public static boolean test_original_remove_add_find(){
+        
         try {
-            byte[] bytes = new byte[(int)file.length()];
-            file.seek(0);
-            file.read(bytes);
-            System.out.println(new String(bytes));
-        } catch (IOException ex) {
-            Logger.getLogger(UnitTests.class.getName()).log(Level.SEVERE, null, ex);
+            original.write("hellogoodbye".getBytes());
         }
-    }
-}
+        catch (IOException ex) {
+            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        piecetable.add_original(12);
+        assert new String(piecetable.get_text()).equals(new String(piecetable.find(0,piecetable.byte_length))) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellogoodbye"): "an edit sequence occured with an error";
+        //"hellogoodbye"
+        
+        piecetable.remove(piecetable.byte_length-7, 3);
+        assert new String(piecetable.get_text()).equals(new String(piecetable.find(0,piecetable.byte_length))) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellodbye"): "an edit sequence occured with an error";
+        //"hellodbye"
+        
+        piecetable.undo();
+        assert new String(piecetable.get_text()).equals(new String(piecetable.find(0,piecetable.byte_length))) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellogoodbye"): "an edit sequence occured with an error";
+        //"hellogoodbye"
+
+        piecetable.redo();
+        assert new String(piecetable.get_text()).equals(new String(piecetable.find(0,piecetable.byte_length))) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellodbye"): "an edit sequence occured with an error";
+        //"hellodbye"
+        
+        piecetable.undo();
+        assert new String(piecetable.get_text()).equals(new String(piecetable.find(0,piecetable.byte_length))) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellogoodbye"): "an edit sequence occured with an error";
+        //"hellogoodbye"
+        
+        try {
+            edits.seek(edits.length());
+            edits.write("adding I love programming".getBytes());
+        }
+        catch (IOException ex) {
+            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        piecetable.add(25, (int) piecetable.byte_length);
+        assert new String(piecetable.find(0,piecetable.byte_length)).equals(new String (piecetable.get_text())) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellogoodbyeadding I love programming"): "an edit sequence occured with an error";
+        //"hellogoodbyeadding I love programming"
+        
+        piecetable.remove(piecetable.byte_length-7, 3);
+        assert new String(piecetable.find(0,piecetable.byte_length)).equals(new String (piecetable.get_text())) : "get_text() != find(0, length)";
+        assert new String(piecetable.get_text()).equals("hellogoodbyeadding I love progming"): "an edit sequence occured with an error";
+        //"hellogoodbyeadding I love progming
