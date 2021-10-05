e2180aa8da95eed18f04f86d29838903d7bf38ba
FreddyJohn <39596344+FreddyJohn@users.noreply.github.com>
2021-10-05 17:50:23 -0400

    Update piecetable.java
    fix some logic errors in add

--- piecetable.java
+++ piecetable.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
+/*
  * @author Nick
  * 
  * 
@@ -16,14 +15,16 @@ import java.util.logging.Logger;
 public class PieceTable implements Serializable{
     public long byte_length;
     public ArrayList<_Piece> pieces;
-    public long _index;
+    public ArrayList<_Piece> redoStack;
+    public ArrayList<_Piece> undoStack;
+    public long index;
     public PieceTable(){
         pieces = new ArrayList<>();
     }
     public void print_pieces(){
         System.out.println("PieceTable Pieces Stack");
         this.pieces.forEach((piece) -> {
-            System.out.println(piece.in_added+","+piece.length+","+piece.offset);
+            System.out.println(piece.in_added+","+piece.length+","+piece.sequenceIndex);
         });
     }
     public long get_length(RandomAccessFile f){
@@ -55,7 +56,7 @@ public class PieceTable implements Serializable{
         }
         return list;
     }
-    private Pair get_pieces_and_offset(long index){
+    private Pair get_pieces_and_offset(long index, int length){
         if (index<0){
            return null;
         }
@@ -63,36 +64,45 @@ public class PieceTable implements Serializable{
         for(int i=0;i<pieces.size();i++){
             _Piece p=pieces.get(i);
             if (remainingOffset <= p.length){
-                return new Pair(i, p.offset + remainingOffset);
+                return new Pair(i, p.sequenceIndex + remainingOffset);
             }    
             remainingOffset -= p.length;   
         }
         return null;
     }
-    public void add_original(int length){
+    
+    public void add_original(int offset, int length){
+        pieces.add(new _Piece(false,offset,length));
         byte_length = length;
-        pieces.add(new _Piece(false,0,length));
     }
-    public PieceTable add(int length,int index, RandomAccessFile edits){
+    public PieceTable add(int length,int index, RandomAccessFile buffer){
         if (length==0){
             return this;
         }
-        Pair pair = get_pieces_and_offset(index);
+        Pair pair = get_pieces_and_offset(index,length);
         int piece_index = (int) pair.first;
         long piece_offset= (long) pair.second;
-        _Piece curr_piece = pieces.get(piece_index);        
-        _index+=length;
-        long added_offset = _index - length;
-        byte_length += length;
+        _Piece curr_piece = pieces.get(piece_index);     
         
-        if (curr_piece.in_added && piece_offset == curr_piece.offset + (curr_piece.length == added_offset ? 1:0)){
+        try {
+            this.index=buffer.length();
+        } catch (IOException ex) {
+            Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        long added_offset = this.index - length;
+        byte_length += length;
+        /*
+        if (curr_piece.in_added & piece_offset == curr_piece.sequenceIndex + (curr_piece.length == added_offset ? 1:0)){
+            System.out.println("this");
             curr_piece.length += length;
             return this;
-        }
+        }*/
+        
+                
         ArrayList<_Piece> insert_pieces = new ArrayList<>();
-        insert_pieces.add(new _Piece(curr_piece.in_added,curr_piece.offset, piece_offset - curr_piece.offset));
+        insert_pieces.add(new _Piece(curr_piece.in_added,curr_piece.sequenceIndex, piece_offset - curr_piece.sequenceIndex));
         insert_pieces.add(new _Piece(true, added_offset, length));
-        insert_pieces.add(new _Piece(curr_piece.in_added,piece_offset,curr_piece.length-(piece_offset - curr_piece.offset)));
+        insert_pieces.add(new _Piece(curr_piece.in_added,piece_offset,curr_piece.length-(piece_offset - curr_piece.sequenceIndex)));
         insert_pieces = filter(insert_pieces);
         pieces = splice(piece_index,1,insert_pieces);
         return this;
@@ -101,11 +111,9 @@ public class PieceTable implements Serializable{
         ByteBuffer doc = ByteBuffer.allocate((int) byte_length);
         pieces.forEach((piece) -> {
             if (piece.in_added){
-                doc.put(get_chunk(_edits, piece.offset, piece.offset + piece.length));
-                //System.out.println(piece.offset+" , "+piece.length);
+                doc.put(get_chunk(_edits, piece.sequenceIndex, piece.sequenceIndex + piece.length));
             }else{
-                doc.put(get_chunk(_origPiece, piece.offset, piece.offset + piece.length));
-                //System.out.println(piece.offset+" , "+piece.length);
+                doc.put(get_chunk(_origPiece, piece.sequenceIndex, piece.sequenceIndex + piece.length));
             }
         });
         return doc.array();
@@ -123,11 +131,11 @@ public class PieceTable implements Serializable{
     }
     public byte[] find(long index,long length,RandomAccessFile _edits,RandomAccessFile origPiece){
         if(length<0){
-            return find(index+length, -length, _edits, origPiece);
+            return null;
         }
         ByteBuffer doc = ByteBuffer.allocate((int) length);
-        Pair start_pair = get_pieces_and_offset(index);
-        Pair stop_pair = get_pieces_and_offset(index+length);
+        Pair start_pair = get_pieces_and_offset(index, (int) length);
+        Pair stop_pair = get_pieces_and_offset(index+length, (int) length);
         int start_piece_index=(int)start_pair.first;
         long start_piece_offset=(long)start_pair.second;
         int stop_piece_index=(int)stop_pair.first;
@@ -135,23 +143,21 @@ public class PieceTable implements Serializable{
         
         _Piece start_piece = pieces.get(start_piece_index);
         RandomAccessFile buffer = start_piece.in_added ? _edits : origPiece;
-        
         if(start_piece_index==stop_piece_index){
             doc.put(get_chunk(buffer,start_piece_offset,start_piece_offset + length));
         }
         else{
-            doc.put(get_chunk(buffer,start_piece_offset,start_piece.offset + start_piece.length));
+            doc.put(get_chunk(buffer,start_piece_offset,start_piece.sequenceIndex + start_piece.length));
             for(int i =start_piece_index+1;i<stop_piece_index+1;i++){
                 _Piece cur_piece=pieces.get(i);
                 buffer = cur_piece.in_added ? _edits : origPiece;
                 if (i==stop_piece_index){
-                    doc.put(get_chunk(buffer,cur_piece.offset,stop_piece_offset));
+                    doc.put(get_chunk(buffer,cur_piece.sequenceIndex,stop_piece_offset));
                 }
                 else{
-                    doc.put(get_chunk(buffer,cur_piece.offset,cur_piece.offset+cur_piece.length));
+                    doc.put(get_chunk(buffer,cur_piece.sequenceIndex,cur_piece.sequenceIndex+cur_piece.length));
                 }
             }
-
         }
         return doc.array();
     }
@@ -159,9 +165,7 @@ public class PieceTable implements Serializable{
         if(length==0){
             return this;
         }
-        if(length<0){
-            remove(index+length,-length);
-        }
+
         if(index<0){
             try {
                 throw new Exception("Index out of Bounds");
@@ -169,31 +173,31 @@ public class PieceTable implements Serializable{
                 Logger.getLogger(PieceTable.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        Pair start_pair = get_pieces_and_offset(index);
-        Pair stop_pair = get_pieces_and_offset(index+length);
+        Pair start_pair = get_pieces_and_offset(index, (int) length);
+        Pair stop_pair = get_pieces_and_offset(index+length, (int) length);
         int start_piece_index=(int)start_pair.first;
         long start_piece_offset=(long)start_pair.second;
         int stop_piece_index=(int)stop_pair.first;
         long stop_piece_offset=(long)stop_pair.second;
         byte_length -= length;
-        _index -= length;
         if (start_piece_index == stop_piece_index){
             _Piece piece = pieces.get(start_piece_index);
-            if (start_piece_offset==piece.offset){
-                piece.offset+=length;
+            if (start_piece_offset==piece.sequenceIndex){
+                piece.sequenceIndex+=length;
                 piece.length-=length;
                 return this;
             }
-            else if (stop_piece_offset == piece.offset+piece.length){
+            else if (stop_piece_offset == piece.sequenceIndex+piece.length){
                 piece.length-=length;
                 return this;
             }
         }
+        
         _Piece start_piece = pieces.get(start_piece_index);
         _Piece end_piece = pieces.get(stop_piece_index);
         ArrayList<_Piece> delete_pieces = new ArrayList<>();
-        delete_pieces.add(new _Piece(start_piece.in_added,start_piece.offset, start_piece_offset - start_piece.offset));
-        delete_pieces.add(new _Piece(end_piece.in_added, stop_piece_offset, end_piece.length -(stop_piece_offset-end_piece.offset)));
+        delete_pieces.add(new _Piece(start_piece.in_added,start_piece.sequenceIndex, start_piece_offset - start_piece.sequenceIndex));
+        delete_pieces.add(new _Piece(end_piece.in_added, stop_piece_offset, end_piece.length -(stop_piece_offset-end_piece.sequenceIndex)));
         delete_pieces = filter(delete_pieces);
         int delete_count = stop_piece_index - start_piece_index + 1;
         pieces = splice(start_piece_index,delete_count,delete_pieces);
@@ -201,12 +205,14 @@ public class PieceTable implements Serializable{
     }
        public static class _Piece implements Serializable{
         public boolean in_added;
-        public long offset;
+        public long sequenceIndex;
         public long length;
-        public _Piece(boolean in_added, long offset, long length){
+        public _Piece(boolean in_added, long offset,
+                long length){
             this.in_added=in_added;
-            this.offset=offset;
+            this.sequenceIndex=offset;
             this.length=length;
+              
         }
     }
 }
